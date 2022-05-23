f97a84239c354d9a852431a25969a1b71c67333a
CityscapeSegmentation <106041182+CityscapeSegmentation@users.noreply.github.com>
2022-05-23 08:07:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,37 @@ uploaded_file = st.file_uploader("Choose a file")
 col1, col2 = st.columns(2)
 
 
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+# # A button to decrement the counter
+# decrement = st.button('Decrement')
+# if decrement:
+#     st.session_state.count -= 1
+
+
 with col1:
-  if uploaded_file is not None:
-    image=Image.open(uploaded_file)
-    #st.write(uploaded_file)
-    st.image(image, caption='Sunrise by the mountains')
-    st.write('👈  Please upload an image ')
+  decrement = st.button('Decrement')
+   if decrement:
+	st.session_state.count -= 1
+   if st.session_state.count<1:
+	target=st.session_state.count=1
+
+with col2:
+  increment = st.button('Increment')
+   if increment:
+	st.session_state.count += 1
+   if st.session_state.count>500:
+	target=st.session_state.count=500
+	
+rgb_path='data/val/rgb/'+str(target)+'jpg'
+mask_path='data/val/mask/'+str(target)+'jpg'
+  
+rgb=Image.open(rgb_path)
+mask=Image.open(mask_path)
+
+#st.write(uploaded_file)
+st.image(image, caption='Sunrise by the mountains')
+st.write('👈  Please upload an image ')
 
