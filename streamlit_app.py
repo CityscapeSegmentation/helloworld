5e52141bc72b6f8658194bf787f2b7a345cd6b1a
CityscapeSegmentation <106041182+CityscapeSegmentation@users.noreply.github.com>
2022-05-23 04:12:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,55 @@
-import streamlit as st
-from datetime import time, datetime
-import torch
-import matplotlib
-import numpy as np
-import PIL
+# import streamlit as st
+# from datetime import time, datetime
+# import torch
+# import matplotlib
+# import numpy as np
+# import PIL
 
 
-st.header('st.slider')
+# st.header('st.slider')
 
-# Example 1
+# # Example 1
 
-st.subheader('Slider')
+# st.subheader('Slider')
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+# age = st.slider('How old are you?', 0, 130, 25)
+# st.write("I'm ", age, 'years old')
 
-# Example 2
+# # Example 2
 
-st.subheader('Range slider')
+# st.subheader('Range slider')
 
-values = st.slider(
-     'Select a range of values',
-     0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
+# values = st.slider(
+#      'Select a range of values',
+#      0.0, 100.0, (25.0, 75.0))
+# st.write('Values:', values)
 
-# Example 3
+# # Example 3
 
-st.subheader('Range time slider')
+# st.subheader('Range time slider')
 
-appointment = st.slider(
-     "Schedule your appointment:",
-     value=(time(11, 30), time(12, 45)))
-st.write("You're scheduled for:", appointment)
+# appointment = st.slider(
+#      "Schedule your appointment:",
+#      value=(time(11, 30), time(12, 45)))
+# st.write("You're scheduled for:", appointment)
 
-# Example 4
+# # Example 4
 
-st.subheader('Datetime slider')
+# st.subheader('Datetime slider')
 
-start_time = st.slider(
-     "When do you start?",
-     value=datetime(2020, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
+# start_time = st.slider(
+#      "When do you start?",
+#      value=datetime(2020, 1, 1, 9, 30),
+#      format="MM/DD/YY - hh:mm")
+# st.write("Start time:", start_time)
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
