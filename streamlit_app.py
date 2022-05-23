f135f729e72309c650162fe8204935600f538063
CityscapeSegmentation <106041182+CityscapeSegmentation@users.noreply.github.com>
2022-05-23 06:09:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,6 @@ uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
   image=Image.open(uploaded_file)
+  st.write(uploaded_file)
   st.image(image, caption='Sunrise by the mountains')
 
