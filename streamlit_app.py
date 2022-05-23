cc78d41fe0a5afbe7bf7ec9d216847fcf8fe505d
CityscapeSegmentation <106041182+CityscapeSegmentation@users.noreply.github.com>
2022-05-23 06:16:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,15 @@ import numpy as np
 from  PIL import Image
 
 
-
 uploaded_file = st.file_uploader("Choose a file")
 
+col1, col2, col3 = st.columns(3)
+
+
+with col1:
 if uploaded_file is not None:
   image=Image.open(uploaded_file)
   st.write(uploaded_file)
   st.image(image, caption='Sunrise by the mountains')
+  st.write('👈  Please upload an image ')
 
