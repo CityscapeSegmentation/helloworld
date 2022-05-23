a12fddab3600418babe5d579d1950afbb005efb5
CityscapeSegmentation <106041182+CityscapeSegmentation@users.noreply.github.com>
2022-05-23 13:01:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ if 'count' not in st.session_state:
 if 'model' not in st.session_state:
 	st.session_state.model = UNet(15)
 	model=st.session_state.model
+	model.cpu()
 	model.load_state_dict(torch.load('weights/best_cpu.pt'))
 	
 
