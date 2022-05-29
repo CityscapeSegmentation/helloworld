d20fd272566270613d30e9e5ecea7b1f861575f7
CityscapeSegmentation <106041182+CityscapeSegmentation@users.noreply.github.com>
2022-05-29 01:13:53 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -45,3 +45,50 @@ class CityDataset(Dataset):
 #DataLoader(val_set, batch_size=1, shuffle=True, num_workers=0)
 
 #image = np.array(Image.open( data_path+'/rgb/'+ self.input_images[idx])  )
+
+tmp=[
+[0,0,0],
+[0,0,0],
+[0,0,0],
+[0,0,0],
+[0,0,0],
+[111,74,0],
+[81,0,81],
+[128,64,128],
+[244,35,232],
+[250,170,160],
+[230,150,140],
+[70,70,70],
+[102,102,156],
+[190,153,153],
+[180,165,180],
+[150,100,100],
+[150,120,90],
+[153,153,153],
+[153,153,153],
+[250,170,30],
+[220,220,0],
+[107,142,35],
+[152,251,152],
+[70,130,180],
+[220,20,60],
+[255,0,0],
+[0,0,142],
+[0,0,70],
+[0,60,100],
+[0,0,90],
+[0,0,110],
+[0,80,100],
+[0,0,230],
+[119,11,32],
+[0,0,142]]
+
+###remove repeated colors
+givin_colors=[]
+for c in tmp:
+    if not c in givin_colors:
+        givin_colors.append(c)
+        #print(c)
+
+givin_colors=np.array(givin_colors)
+#print(givin_colors.shape)
